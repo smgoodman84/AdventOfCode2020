9bba40aaaaf4b7cf4accd2d95223e7184eb5e346
Simon Goodman <simon.goodman84@googlemail.com>
2020-12-01 19:38:52 +0000

    Add Day 1 Part 2

--- AdventOfCode2020/Day01/ExpenseReport.cs
+++ AdventOfCode2020/Day01/ExpenseReport.cs
@@ -1,5 +1,6 @@
-﻿using System.IO;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace AdventOfCode2020.Day01
@@ -34,6 +35,57 @@ namespace AdventOfCode2020.Day01
             var otherItem = sumToFind - requiredItem;
 
             return requiredItem * otherItem;
+        }
+
+        public int GetPart2Result()
+        {
+            var sumToFind = 2020;
+
+            foreach (var expense in _expenses)
+            {
+                var subSumToFind = sumToFind - expense;
+                var subExpenses = _expenses.Where(e => e != expense);
+
+                var subResult = FindSumItems(subSumToFind, subExpenses);
+                if (subResult.Found)
+                {
+                    return subResult.Item1 * subResult.Item2 * expense;
+                }
+            }
+
+            throw new Exception("Could not find result");
+        }
+
+        private SumItemsResult FindSumItems(int sumToFind, IEnumerable<int> items)
+        {
+            var hashedItems = new HashSet<int>(items);
+
+            var itemSums = items.Select(e => sumToFind - e);
+
+            foreach (var itemSum in itemSums)
+            {
+                if (hashedItems.Contains(itemSum))
+                {
+                    return new SumItemsResult
+                    {
+                        Found = true,
+                        Item1 = itemSum,
+                        Item2 = sumToFind - itemSum
+                    };
+                }
+            }
+
+            return new SumItemsResult
+            {
+                Found = false
+            };
+        }
+
+        private class SumItemsResult
+        {
+            public bool Found { get; set; }
+            public int Item1 { get; set; }
+            public int Item2 { get; set; }
         }
     }
 }
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -7,11 +7,17 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            var day1 = ExpenseReport
+            var day1Part1 = ExpenseReport
                 .LoadFromFile("Day01/ExpenseReport.txt")
                 .GetResult();
 
-            Console.WriteLine($"Day1: {day1}");
+            Console.WriteLine($"Day 1 Part 1: {day1Part1}");
+
+            var day1Part2 = ExpenseReport
+                .LoadFromFile("Day01/ExpenseReport.txt")
+                .GetPart2Result();
+
+            Console.WriteLine($"Day 1 Part 2: {day1Part2}");
         }
     }
 }
