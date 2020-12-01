f619275f19167be3f8a0c0313b87c1b9267f6ec0
Simon Goodman <simon.goodman84@googlemail.com>
2020-12-01 19:24:40 +0000

    Day 1 Part 1

---
+++ AdventOfCode2020/Day01/ExpenseReport.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day01
+{
+    public class ExpenseReport
+    {
+        private IEnumerable<int> _expenses;
+        public ExpenseReport(IEnumerable<int> expenses)
+        {
+            _expenses = expenses;
+        }
+
+        public static ExpenseReport LoadFromFile(string filename)
+        {
+            var expenses = File.ReadAllLines(filename)
+                .Select(l => int.Parse(l))
+                .ToList();
+
+            return new ExpenseReport(expenses);
+        }
+
+        public int GetResult()
+        {
+            var sumToFind = 2020;
+
+            var hashedExpenses = new HashSet<int>(_expenses);
+
+            var expenseSums = _expenses.Select(e => sumToFind - e);
+
+            var requiredItem = expenseSums.First(hashedExpenses.Contains);
+
+            var otherItem = sumToFind - requiredItem;
+
+            return requiredItem * otherItem;
+        }
+    }
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AdventOfCode2020.Day01;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day1 = ExpenseReport
+                .LoadFromFile("Day01/ExpenseReport.txt")
+                .GetResult();
+
+            Console.WriteLine($"Day1: {day1}");
+        }
+    }
+}
