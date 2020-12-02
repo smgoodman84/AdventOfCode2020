47ee42fc5d89b7f624276b410cde13b99c7d9d80
Simon Goodman <simon.goodman84@googlemail.com>
2020-12-02 06:09:39 +0000

    Day 2 Part 1

---
+++ AdventOfCode2020/Day02/PasswordValidator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020.Day02
+{
+    public class PasswordValidator
+    {
+        private IEnumerable<PasswordWithPolicy> _passwordPolicies;
+        private PasswordValidator(IEnumerable<PasswordWithPolicy> passwordPolicies)
+        {
+            _passwordPolicies = passwordPolicies;
+        }
+
+        public static PasswordValidator LoadFromFile(string filename)
+        {
+            var expenses = File.ReadAllLines(filename)
+                .Select(ParseLine)
+                .ToList();
+
+            return new PasswordValidator(expenses);
+        }
+
+        private static PasswordWithPolicy ParseLine(string line)
+        {
+            var policyPasswordSplit = line.Split(':');
+            var policy = policyPasswordSplit[0].Trim();
+            var password = policyPasswordSplit[1].Trim();
+
+            var requiredCharacterSplit = policy.Split(' ');
+            var minMax = requiredCharacterSplit[0].Trim();
+            var requiredCharacter = requiredCharacterSplit[1].Trim()[0];
+
+            var minMaxSplit = minMax.Split('-');
+            var min = int.Parse(minMaxSplit[0].Trim());
+            var max = int.Parse(minMaxSplit[1].Trim());
+
+            return new PasswordWithPolicy
+            {
+                Password = password,
+                RequiredCharacter = requiredCharacter,
+                MinOccurences = min,
+                MaxOccurences = max
+            };
+        }
+
+        public int Part1()
+        {
+            return _passwordPolicies.Count(IsValid);
+        }
+
+        private bool IsValid(PasswordWithPolicy passwordWithPolicy)
+        {
+            var count = passwordWithPolicy.Password
+                .Count(c => c == passwordWithPolicy.RequiredCharacter);
+
+            return passwordWithPolicy.MinOccurences <= count
+                && count <= passwordWithPolicy.MaxOccurences;
+        }
+
+        private class PasswordWithPolicy
+        {
+            public string Password { get; set; }
+            public char RequiredCharacter { get; set; }
+            public int MinOccurences { get; set; }
+            public int MaxOccurences { get; set; }
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using AdventOfCode2020.Day01;
+using AdventOfCode2020.Day02;
 
 namespace AdventOfCode2020
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            Day1();
+            Day2();
+        }
+
+        static void Day1()
         {
             var day1Part1 = ExpenseReport
                 .LoadFromFile("Day01/ExpenseReport.txt")
@@ -18,6 +25,15 @@ namespace AdventOfCode2020
                 .GetPart2Result();
 
             Console.WriteLine($"Day 1 Part 2: {day1Part2}");
+        }
+
+        static void Day2()
+        {
+            var day2Part1 = PasswordValidator
+                .LoadFromFile("Day02/Passwords.txt")
+                .Part1();
+
+            Console.WriteLine($"Day 2 Part 1: {day2Part1}");
         }
     }
 }
