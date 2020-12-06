d6874d7b765b06164721dee1a715d9933ba872b4
Simon Goodman <simon.goodman84@googlemail.com>
2020-12-06 05:21:39 +0000

    Day 6

---
+++ AdventOfCode2020/Day06/Declerations.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020.Day06
+{
+    public class Declerations : IDay
+    {
+        public int DayNumber => 6;
+
+        private List<GroupDecleration> _groupDeclerations;
+        private Declerations(List<GroupDecleration> groupDeclerations)
+        {
+            _groupDeclerations = groupDeclerations;
+        }
+
+        public static Declerations LoadFromFile(string filename)
+        {
+            var groupDeclerations = new List<GroupDecleration>();
+            var lines = File.ReadAllLines(filename);
+
+            var currentLines = new List<string>();
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    groupDeclerations.Add(CreateGroupDecleration(currentLines));
+                    currentLines = new List<string>();
+                }
+                else
+                {
+                    currentLines.Add(line);
+                }
+            }
+
+            if (currentLines.Any())
+            {
+                groupDeclerations.Add(CreateGroupDecleration(currentLines));
+            }
+
+
+            return new Declerations(groupDeclerations);
+        }
+
+        private static GroupDecleration CreateGroupDecleration(List<string> lines)
+        {
+            var individualDeclerations = lines
+                .Select(l => new IndividualDecleration
+                {
+                    YesAnswers = l.Trim().ToCharArray()
+                })
+                .ToList();
+
+            var groupDecleration = new GroupDecleration
+            {
+                IndividualDeclerations = individualDeclerations
+            };
+
+            return groupDecleration;
+        }
+
+        public string Part1()
+        {
+            return _groupDeclerations
+                .Sum(gd => gd.GetUniqueYesCount())
+                .ToString();
+        }
+
+        public string Part2()
+        {
+            return _groupDeclerations
+                .Sum(gd => gd.GetConsistentYesCount())
+                .ToString();
+        }
+
+        private class GroupDecleration
+        {
+            public List<IndividualDecleration> IndividualDeclerations { get; set; }
+
+            public int GetUniqueYesCount()
+            {
+                return IndividualDeclerations
+                    .SelectMany(id => id.YesAnswers)
+                    .Distinct()
+                    .Count();
+            }
+
+            public int GetConsistentYesCount()
+            {
+                var uniqueAnswers = 
+                    IndividualDeclerations
+                    .SelectMany(id => id.YesAnswers)
+                    .Distinct();
+
+                var allYes = uniqueAnswers
+                    .Where(a => IndividualDeclerations.All(d => d.YesAnswers.Contains(a)))
+                    .ToList();
+
+                return allYes.Count();
+            }
+        }
+
+        private class IndividualDecleration
+        {
+            public char[] YesAnswers { get; set; }
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -5,6 +5,7 @@ using AdventOfCode2020.Day02;
 using AdventOfCode2020.Day03;
 using AdventOfCode2020.Day04;
 using AdventOfCode2020.Day05;
+using AdventOfCode2020.Day06;
 
 namespace AdventOfCode2020
 {
@@ -18,7 +19,8 @@ namespace AdventOfCode2020
                 PasswordValidator.LoadFromFile("Day02/Passwords.txt"),
                 Map.LoadFromFile("Day03/Map.txt"),
                 PassportValidator.LoadFromFile("Day04/PassportData.txt"),
-                BoardingPasses.LoadFromFile("Day05/Seats.txt")
+                BoardingPasses.LoadFromFile("Day05/Seats.txt"),
+                Declerations.LoadFromFile("Day06/Declerations.txt")
             };
 
             foreach (var day in days)
