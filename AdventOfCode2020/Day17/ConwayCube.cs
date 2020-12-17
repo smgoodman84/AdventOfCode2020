91de16697d2547450f0e415acfa6a865a3ec0586
Simon Goodman <simon.goodman84@googlemail.com>
2020-12-17 06:11:23 +0000

    Add Day 17 Part 1

---
+++ AdventOfCode2020/Day17/ConwayCube.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020.Day17
+{
+    public class ConwayCube : IDay
+    {
+        public int DayNumber => 17;
+        public string ValidatedPart1 => "346";
+        public string ValidatedPart2 => string.Empty;
+
+        private const char Active = '#';
+
+        private Space _space;
+        private ConwayCube(Space space)
+        {
+            _space = space;
+        }
+
+        public static ConwayCube LoadFromFile(string filename)
+        {
+            var space = new Space(0);
+
+            var lines = File.ReadAllLines(filename);
+
+            var y = 0;
+            foreach(var line in lines)
+            {
+                var x = 0;
+                foreach (var c in line.ToCharArray())
+                {
+                    space.SetLocation(x, y, 0, c == Active);
+                    x += 1;
+                }
+                y += 1;
+            }
+
+
+            return new ConwayCube(space);
+        }
+
+        public string Part1() => GetGeneration(6).CountActive().ToString();
+
+        public string Part2()
+        {
+            return string.Empty;
+        }
+
+        private Space GetGeneration(int generation)
+        {
+            var space = _space;
+            while (space.Generation < generation)
+            {
+                space = space.GetNextGeneration();
+            }
+            return space;
+        }
+
+        private class Space
+        {
+            public int Generation { get; private set; }
+            private int _minX = 0;
+            private int _minY = 0;
+            private int _minZ = 0;
+            private int _maxX = 0;
+            private int _maxY = 0;
+            private int _maxZ = 0;
+
+            private Dictionary<int, Dictionary<int, Dictionary<int, bool>>> _space;
+            public Space(int generation)
+            {
+                Generation = generation;
+                _space = new Dictionary<int, Dictionary<int, Dictionary<int, bool>>>();
+            }
+
+            public void SetLocation(int x, int y, int z, bool active)
+            {
+                if (!_space.ContainsKey(x))
+                {
+                    _space.Add(x, new Dictionary<int, Dictionary<int, bool>>());
+                }
+
+                if (!_space[x].ContainsKey(y))
+                {
+                    _space[x].Add(y, new Dictionary<int, bool>());
+                }
+
+                _space[x][y][z] = active;
+
+                if (active)
+                {
+                    if (x < _minX)
+                    {
+                        _minX = x;
+                    }
+
+                    if (x > _maxX)
+                    {
+                        _maxX = x;
+                    }
+
+                    if (y < _minY)
+                    {
+                        _minY = y;
+                    }
+
+                    if (y > _maxY)
+                    {
+                        _maxY = y;
+                    }
+
+                    if (z < _minZ)
+                    {
+                        _minZ = z;
+                    }
+
+                    if (z > _maxZ)
+                    {
+                        _maxZ = z;
+                    }
+                }
+            }
+
+            public bool GetLocation(int x, int y, int z)
+            {
+                if (!_space.ContainsKey(x))
+                {
+                    return false;
+                }
+
+                if (!_space[x].ContainsKey(y))
+                {
+                    return false;
+                }
+
+                if (!_space[x][y].ContainsKey(z))
+                {
+                    return false;
+                }
+
+                return _space[x][y][z];
+            }
+
+            public Space GetNextGeneration()
+            {
+                var nextGeneration = new Space(Generation + 1);
+                for (var x = _minX - 1; x <= _maxX + 1; x++)
+                {
+                    for (var y = _minY - 1; y <= _maxY + 1; y++)
+                    {
+                        for (var z = _minZ - 1; z <= _maxZ + 1; z++)
+                        {
+                            var active = GetLocation(x, y, z);
+                            var count = CountNeighbours(x, y, z);
+                            var newState = GetNewState(active, count);
+                            nextGeneration.SetLocation(x, y, z, newState);
+                        }
+                    }
+                }
+                return nextGeneration;
+            }
+
+            private int CountNeighbours(int x, int y, int z)
+            {
+                var count = 0;
+                for (var currentX = x - 1; currentX <= x + 1; currentX++)
+                {
+                    for (var currentY = y - 1; currentY <= y + 1; currentY++)
+                    {
+                        for (var currentZ = z - 1; currentZ <= z + 1; currentZ++)
+                        {
+                            if (!(currentX == x && currentY == y && currentZ == z))
+                            {
+                                if (GetLocation(currentX, currentY, currentZ))
+                                {
+                                    count += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                return count;
+            }
+
+            private bool GetNewState(bool active, int count)
+            {
+                if (active)
+                {
+                    return count == 2 || count == 3;
+                }
+
+                return count == 3;
+            }
+
+            public int CountActive()
+            {
+                var count = 0;
+                foreach (var x in _space.Keys)
+                {
+                    foreach (var y in _space[x].Keys)
+                    {
+                        foreach (var z in _space[x][y].Keys)
+                        {
+                            if (_space[x][y][z])
+                            {
+                                count += 1;
+                            }
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
